16ef9f871eeb20731f7ba3b8c5d8eafe21490c55
AnjumSunkesula <anjumsunkesula@gmail.com>
2026-02-03 07:56:02 +0530

    Update AWS region and email in config

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'sangameshbhai')
-    AWS_REGION = 'ap-south-2'
+    AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "sbiradar2185@gmail.com"
+    EMAIL = "anjumsunkesula@gmail.com"
